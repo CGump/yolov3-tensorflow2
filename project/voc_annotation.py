ffbdfe5ff8c7c053a15722f60220553d648215b0
cgump <824592076@qq.com>
2020-07-11 16:21:22 +0800

    finish voc_annotation

--- project/voc_annotation.py
+++ project/voc_annotation.py
@@ -1,40 +1,45 @@
 import xml.etree.ElementTree as ET
 from os import getcwd
 
-sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
-classes_path = "./data/voc_classes.txt"
 
-def get_classes(classes_path):
-    with open(classes_path) as f:
+def get_classes(path):
+    with open(path) as f:
         class_names = f.readlines()
     class_names = [c.strip() for c in class_names]
     return class_names
 
-classes = get_classes(classes_path)
 
-def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    tree=ET.parse(in_file)
+def convert_annotation(years, image_num, files):
+    in_file = open(f"VOCdevkit/VOC{years}/Annotations/{image_num}.xml")
+    tree = ET.parse(in_file)
     root = tree.getroot()
 
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
-        if cls not in classes or int(difficult)==1:
+        if cls not in classes or int(difficult) == 1:
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
-        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
-        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+        box_axis = (int(xmlbox.find('xmin').text),
+                    int(xmlbox.find('ymin').text),
+                    int(xmlbox.find('xmax').text),
+                    int(xmlbox.find('ymax').text))
+        files.write(" " + ",".join([str(value) for value in box_axis]) + ',' + str(cls_id))
 
-wd = getcwd()
 
-for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
-    for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
-        convert_annotation(year, image_id, list_file)
-        list_file.write('\n')
-    list_file.close()
+if __name__ == '__main__':
+    sets = [('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+    classes_path = "./data/voc_classes.txt"
+    classes = get_classes(classes_path)
+    wd = getcwd()
+
+    for year, image_set in sets:
+        image_ids = open(f"VOCdevkit/VOC{year}/ImageSets/Main/{image_set}.txt").read().strip().split()
+        list_file = open(f"data/{year}_{image_set}.txt", 'w')
+        for image_id in image_ids:
+            list_file.write(f"{wd}/VOCdevkit/VOC{year}/JPEGImages/{image_id}.jpg")
+            convert_annotation(year, image_id, list_file)
+            list_file.write('\n')
+        list_file.close()
 
